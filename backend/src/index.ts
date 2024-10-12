c8d1a9f13788d8b40ad8377bc47c409b20c80293
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-10-12 13:43:55 +0200

    simplyfied the api and made a folder for the pokemon list

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,28 @@
-import { server } from './api';
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import { pokemonList } from './pokemonData';
 
+const app = express();
 const PORT = 3000;
-const HOST = '0.0.0.0';
 
-server.listen(PORT, HOST, () => {
-  console.log(`Server is up and and running http://${HOST}:${PORT}`);
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/pokemon/:number', (req: Request, res: Response) => {
+    const pokemonNumber = parseInt(req.params.number, 10);
+
+
+    const pokemon = pokemonList.find(p => p.number === pokemonNumber);
+
+    if (pokemon) {
+        res.status(200).json(pokemon);
+    } else {
+        res.status(404).json({ error: 'Pokémon not found' });
+    }
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
---
+++ backend/src/pokemonData.ts
@@ -0,0 +1,8 @@
+
+export const pokemonList = [
+    { number: 1, name: "Bulbasaur", type: ["Grass", "Poison"], abilities: ["Overgrow"], image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" },
+    { number: 2, name: "Ivysaur", type: ["Grass", "Poison"], abilities: ["Overgrow"], image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png" },
+    { number: 3, name: "Venusaur", type: ["Grass", "Poison"], abilities: ["Overgrow"], image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png" },
+
+  ];
+  
